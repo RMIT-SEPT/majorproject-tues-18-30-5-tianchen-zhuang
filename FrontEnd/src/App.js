567b5f21a05331cb63866bcd0d50b8ebbd665770
s3723037 <s3723037@student.rmit.edu.au>
2020-08-09 16:24:28 +0900

    frontend environment setup

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Login from './component/login/login'
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Login></Login>
+
+    </div>
+  );
+}
+
+export default App;
