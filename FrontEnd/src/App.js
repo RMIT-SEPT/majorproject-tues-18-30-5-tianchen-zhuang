27225c4ee41f405ae43aa3d09f8ca2258cbdb481
s3723037 <s3723037@student.rmit.edu.au>
2020-08-15 14:46:49 +0900

    login front end setup

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import UserComponent from './components/UserComponent';
+ import UserComponent from './components/UserComponent';
+import Login from './components/login/login';
+import 'antd/dist/antd.css';
+
 
 function App() {
   return (
     <div className="App">
-        <UserComponent />
+        {/* <UserComponent /> */}
+        <Login />
     </div>
   );
 }
