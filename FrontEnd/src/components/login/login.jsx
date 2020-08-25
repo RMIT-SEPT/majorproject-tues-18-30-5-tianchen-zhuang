e4dd18c6127dde0c612b1cf66670b6c0e681a17d
s3723037 <s3723037@student.rmit.edu.au>
2020-08-25 14:53:19 +0900

    login and register front-end update

--- FrontEnd/src/components/login/login.jsx
+++ FrontEnd/src/components/login/login.jsx
@@ -4,30 +4,151 @@ import { Card } from 'antd';
 import './login.css'
 import { Input } from 'antd';
 
-class Login extends React.Component{
+
+class Register extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+          input: {},
+          errors: {}
+        };
+         
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+         
+      handleChange(event) {
+        let input = this.state.input;
+        input[event.target.name] = event.target.value;
+      
+        this.setState({
+          input
+        });
+      }
+        
+      handleSubmit(event) {
+        event.preventDefault();
+      
+        if(this.validate()){
+            console.log(this.state);
+      
+            let input = {};
+            input["password"] = "";
+            input["email"] = "";
+            input["username"] = "";
+            input["cPassword"] = "";
+            this.setState({input:input});
+      
+            alert('Thank you for register');
+        }
+      }
+      
+      validate(){
+          let input = this.state.input;
+          let errors = {};
+          let isValid = true;
+      
+      
+          if (!input["email"]) {
+            isValid = false;
+            errors["email"] = "Please enter your email Address.";
+          }
+      
+          if (typeof input["email"] !== "undefined") {
+              
+            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+            if (!pattern.test(input["email"])) {
+              isValid = false;
+              errors["email"] = "Please enter valid email address.";
+            }
+          }
+      
+          if (!input["password"]) {
+            isValid = false;
+            errors["password"] = "Please enter your password.";
+          }
+          if (typeof input["password"] !== "undefined") {
+              
+            var pattern = new RegExp(/^[a-zA-Z0-9]{8,}$/i);
+            if (!pattern.test(input["password"])) {
+              isValid = false;
+              errors["password"] = "Please enter password more than 8 characters";
+            }
+          }
+          if (!input["username"]) {
+            isValid = false;
+            errors["username"] = "Please enter your uername.";
+          }
+          if (!input["cPassword"]) {
+            isValid = false;
+            errors["cPassword"] = "Please enter confirm your password.";
+          }
+          if (input["cPassword"]!=input["password"]) {
+            isValid = false;
+            errors["password"] = "password and confirm password doesnt match";
+          }
+      
+          this.setState({
+            errors: errors
+          });
+      
+          return isValid;
+      }
   render() {
     return (
-        <div className="login">
-            <Card >
-                <Menu mode="horizontal">
-                    <Menu.Item><h3>Login</h3></Menu.Item>
-                </Menu>
-                    {/*<ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>*/}
-                    {/*<ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>*/}
-                    <div className="login-input-row">
-                        <Input placeholder="User Name" name="username"  />
-                    </div>
-                    <div className="login-input-row">
-                        <Input.Password placeholder="Password" name="password" />
-                    </div>
-                    <button className="btn btn-success" >Login</button>
-            </Card>
-        </div>
+        
+        <div>
+        <h2>Please register</h2>
+        <form onSubmit={this.handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          type="text"
+          id="email"
+          placeholder="Enter your email"
+          value={this.state.input.email}
+          onChange={this.handleChange}
+        />
+        <div className="text-danger">{this.state.errors.email}</div>
+        <label htmlFor="email">User Name</label>
+        <input
+          name="username"
+          type="username"
+          id ="username"
+          placeholder="Enter your username"
+          value={this.state.input.username}
+          onChange={this.handleChange}
+        />
+        <div className="text-danger">{this.state.errors.username}</div>
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          id ="password"
+          placeholder="Enter your password"
+          value={this.state.input.password}
+          onChange={this.handleChange}
+        />
+        <div className="text-danger">{this.state.errors.password}</div>
+        <label htmlFor="email">confirm password</label>
+        <input
+          name="cPassword"
+          type="password"
+          id ="cPassword"
+          placeholder="Please confirm your password"
+          value={this.state.input.cPassword}
+          onChange={this.handleChange}
+        />
+        <div className="text-danger">{this.state.errors.cPassword}</div>
+        <button type="submit">Login</button>
+        
+      </form>
+      </div>
 
 
     )
 }
 }
 
-export default Login
+export default Register
 
