e18310f02eb8da0cb3f1260f7f357dc06e3cc3ba
Lachlan Boughton <officiallockie@gmail.com>
2020-08-31 23:34:20 +1000

    Removed commented code in SpringbootBackendApplication.java

--- BackEnd/src/main/java/com/sept/springboot/SpringbootBackendApplication.java
+++ BackEnd/src/main/java/com/sept/springboot/SpringbootBackendApplication.java
@@ -4,19 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication /*implements CommandLineRunner*/ {
-
-    public static void main(String[] args) {
+public class SpringbootBackendApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(SpringbootBackendApplication.class, args);
     }
-/*
-    @Autowired
-    private UserRepository userRepository;
-
-    @Override
-    public void run(String...args) throws Exception {
-        this.userRepository.save(new User("Ramesh", "Fadatare", "ramesh@gmail.com"));
-        this.userRepository.save(new User("Tom", "Cruise", "tom@gmail.com"));
-        this.userRepository.save(new User("Tony", "Stark", "tony@gmail.com"));
-    }*/
 }
