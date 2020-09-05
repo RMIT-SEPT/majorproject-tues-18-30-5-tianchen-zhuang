4f4e94aeccbe12749ffeb92647fb8ab4a91984a6
Lachlan Boughton <officiallockie@gmail.com>
2020-09-05 21:53:32 +1000

    updated UserService.java

--- BackEnd/src/main/java/com/sept/springboot/services/UserService.java
+++ BackEnd/src/main/java/com/sept/springboot/services/UserService.java
@@ -1,5 +1,6 @@
 package com.sept.springboot.services;
 
+import com.sept.springboot.exception.UserNotFoundException;
 import com.sept.springboot.model.User;
 import com.sept.springboot.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,38 @@ public class UserService
 
     public User saveOrUpdateUser(User user)
     {
-        // Business logic
-        return userRepository.save(user);
+        try
+        {
+            return userRepository.save(user);
+        }
+        catch (Exception e)
+        {
+            throw new UserNotFoundException("User ID '" + user.getUserid() + "' already exists");
+        }
+    }
+
+    public User findByUserId(long id)
+    {
+        User user = userRepository.findByUserId(id);
+
+        if(user == null)
+            throw new UserNotFoundException("User ID '" + id + "' does not exist");
+
+        return user;
+    }
+
+    public Iterable<User> findAllUsers()
+    {
+        return userRepository.findAll();
+    }
+
+    public void deleteByUserId(long id)
+    {
+        User user = userRepository.findByUserId(id);
+
+        if(user == null)
+            throw new UserNotFoundException("Cannot user with ID '" + id + "'. This person does not exist");
+
+        userRepository.delete(user);
     }
 }
