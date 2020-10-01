605ebc5da8e9002022d5f96000df9de593ef7500
s3716792 <s3716792@student.rmit.edu.au>
2020-10-01 14:35:37 +1000

    booking history for user

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import UserComponent from './components/UserComponent';
+// import UserComponent from './components/UserComponent';
 import Login from './components/login/login';
 import LoginB from './components/login/loginB';
 import NavBar from './components/utils/navBar.jsx';
@@ -12,6 +12,7 @@ import Register from './components/register/register';
 import RegisterB from './components/register/registerB';
 import booking from './components/booking/booking';
 import bookingList from './components/booking/businessList';
+import bookinghistoryU from './components/booking/bookinghistoryU';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import 'antd/dist/antd.css';
@@ -36,6 +37,7 @@ class App extends Component {
           <Route path="/register" exact={true}component={SelectReg} />
           <Route path="/register/user" exact={true}component={Register} />
           <Route path="/register/business" exact={true}component={RegisterB} />
+          <Route path="/bookingHisotryU" exact={true}component={bookinghistoryU} />
           </Switch>
 
           </div>
---
+++ FrontEnd/src/components/booking/bookinghistoryB.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card } from 'antd';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
+const { Meta } = Card;
+
+
+
+
