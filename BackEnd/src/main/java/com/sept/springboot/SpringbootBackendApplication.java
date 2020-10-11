73b70d19968a0cff7f796da8215a6641568dee1f
Lachlan Boughton <officiallockie@gmail.com>
2020-10-11 21:26:23 +1100

    set default time zone to AEDT

--- BackEnd/src/main/java/com/sept/springboot/SpringbootBackendApplication.java
+++ BackEnd/src/main/java/com/sept/springboot/SpringbootBackendApplication.java
@@ -3,9 +3,18 @@ package com.sept.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SpringbootBackendApplication
 {
+    @PostConstruct
+    void init()
+    {
+        TimeZone.setDefault(TimeZone.getTimeZone("AEDT"));
+    }
+
     public static void main(String[] args)
     {
         SpringApplication.run(SpringbootBackendApplication.class, args);
