0e1d09fc530102c301fdcbea00ea906ac5032261
Lachlan Boughton <officiallockie@gmail.com>
2020-10-18 19:48:35 +1100

    Comments in BookingController

--- BackEnd/src/main/java/com/sept/springboot/controller/BookingController.java
+++ BackEnd/src/main/java/com/sept/springboot/controller/BookingController.java
@@ -13,7 +13,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+/*
+    API that controls all functionality for the bookings of customers.
+ */
 @RestController
 @RequestMapping("/api/booking")
 @CrossOrigin
@@ -31,6 +33,12 @@ public class BookingController
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
 
+    /*
+        This post mapping method makes a new booking for an event.
+        Searches if the customer has booked for this event already.
+        If the customer has not booked for this event, the booking will be add to the database,
+        and the currCapacity variable in the event model will be incremented.
+     */
     @PostMapping
     public ResponseEntity<?> createNewBooking(@Valid @RequestBody Booking booking, BindingResult result)
     {
@@ -53,30 +61,48 @@ public class BookingController
         return new ResponseEntity<>(bookingService.addBooking(booking), HttpStatus.CREATED);
     }
 
+    /*
+        Returns the booking by the id (bookingId) passed through.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<?> getBookingById(@PathVariable("id") long id)
     {
         return new ResponseEntity<>(bookingService.findByBookingId(id), HttpStatus.OK);
     }
 
+    /*
+        Returns all the bookings in the database that have been made to all events.
+        Returned as an iterable list.
+     */
     @GetMapping("/all")
     public Iterable<Booking> getAllBookings()
     {
         return bookingService.findAllBookings();
     }
 
+    /*
+        Returns all the bookings for an event by the id (eventId) passed through.
+        Returned as an iterable list.
+     */
     @GetMapping("/event/{id}")
     public Iterable<Booking> getBookingsByEventId(@PathVariable("id") long id)
     {
         return bookingService.findByEventId(id);
     }
 
+    /*
+        Returns all the bookings made by a customer by the id (customerId) passed through.
+        Returned as an iterable list.
+     */
     @GetMapping("/customer/{id}")
     public Iterable<Booking> getBookingsByCustomerId(@PathVariable("id") long id)
     {
         return bookingService.findByCustomerId(id);
     }
 
+    /*
+        Deletes the booking by the id (bookingId) passed through
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteByBookingId(@PathVariable("id") long id)
     {
